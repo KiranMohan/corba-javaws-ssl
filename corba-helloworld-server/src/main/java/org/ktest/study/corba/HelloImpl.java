da85a96e29af3f9f647baf1c978d3a3b59e5a799
kiran mohan <kiranmohan@123.com>
2014-11-21 20:24:01 +0530

    Added Server and Client code.
    Added Server and Client code. The code can be run with maven itself.

---
+++ corba-helloworld-server/src/main/java/org/ktest/study/corba/HelloImpl.java
@@ -0,0 +1,28 @@
+package org.ktest.study.corba;
+
+import HelloApp.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+
+import java.util.Properties;
+
+class HelloImpl extends HelloPOA {
+  private ORB orb;
+
+  public void setORB(ORB orb_val) {
+    orb = orb_val; 
+  }
+    
+  // implement sayHello() method
+  public String sayHello() {
+    return "\nHello world !!\n";
+  }
+    
+  // implement shutdown() method
+  public void shutdown() {
+    orb.shutdown(false);
+  }
+}
